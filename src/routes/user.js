621b1fd8aff042546de07fecc70fa039522737f4
Ananya Shetty <shettyananya2476@gmail.com>
2026-08-16 02:05:30 +0530

    Update user.js

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,32 +1,6 @@
 const express= require('express');
 const router=express.Router();
 
-// const Aircraft = require('../models/airplane.js');
-// const Flight=require('../models/flight.js');
-// const Airport =require('../models/airport.js');
-// const User=require('../models/user.js');
-
-
-// router.post('/register',async(req,res)=>{
-//     try{
-//         const user1= new User({
-//             name:req.body.name,
-//             email:req.body.email,
-//             role:req.body.role,
-//             password:req.body.password
-//         });
-//         await user1.save();
-//         res.send({ success: true, data: { id, name, email } });
-//     }
-//     catch(err){
-//         console.error('Error saving user data:', err);
-//         res.status(500).render('index', { error: 'Error saving user data' });
-//     }
-
-
-// });
-
-// module.exports=router;
 
 
 const { register } = require('../contollers/user');
@@ -50,23 +24,7 @@ router.post('/register', [
 
 router.post('/login',login);
 
-// router.post('/login',async(req,res)=>{
-//   try{
-//     const {email,password}=req.body;
-//     const user2= await User.findOne({email});
-//     if(!user2) return res.status(400).json({message:"invalid"});
-//     const isMatch=await bcrypt.compare(password,req.password);
-//     if(!isMatch) return res.status(400).json({message:"invalid"});
-//     const payload = { id: user._id, email: user.email };
 
-//     const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '15m' });
-
-//     res.json({ token });
-//   }
-//   catch(err){
-//     res.status(500).json({message:"server error"})
-//   }
-// });
 
 
 
